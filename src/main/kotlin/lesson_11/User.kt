8f894ba27a3903ecc43c511a2676ba6fed61be79
mackarpov <mackarpov@gmail.com>
2025-11-22 19:08:49 +0300

    KS-11-1. Создать класс с данными пользователя и создать 2 экземпляра класса.

--- src/main/kotlin/lesson_11/User.kt
+++ src/main/kotlin/lesson_11/User.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_11
 
 class User(
-    val userId: Int,
-    val userLogin: String,
-    val userPassword: String,
-    val userEmail: String,
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
 ) {
 
 }
--- src/main/kotlin/lesson_11/main.kt
+++ src/main/kotlin/lesson_11/main.kt
@@ -3,28 +3,28 @@ package org.example.lesson_11
 fun main() {
 
     val user1 = User(
-        userId = 1,
-        userLogin = "Login1",
-        userPassword = "Password1",
-        userEmail = "user1@gmail.com"
+        id = 1,
+        login = "Login1",
+        password = "Password1",
+        email = "user1@gmail.com"
     )
 
     val user2 = User(
-        userId = 2,
-        userLogin = "Login2",
-        userPassword = "Password2",
-        userEmail = "user2@gmail.com"
-        )
+        id = 2,
+        login = "Login2",
+        password = "Password2",
+        email = "user2@gmail.com"
+    )
 
-    println("User1 Id = ${user1.userId}")
-    println("User1 Login = ${user1.userLogin}")
-    println("User1 Password = ${user1.userPassword}")
-    println("User1 Email = ${user1.userEmail}")
+    println("User1 Id = ${user1.id}")
+    println("User1 Login = ${user1.login}")
+    println("User1 Password = ${user1.password}")
+    println("User1 Email = ${user1.email}")
 
     println()
 
-    println("User2 Id = ${user2.userId}")
-    println("User2 Login = ${user2.userLogin}")
-    println("User2 Password = ${user2.userPassword}")
-    println("User2 Email = ${user2.userEmail}")
+    println("User2 Id = ${user2.id}")
+    println("User2 Login = ${user2.login}")
+    println("User2 Password = ${user2.password}")
+    println("User2 Email = ${user2.email}")
 }
