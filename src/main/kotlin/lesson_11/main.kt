270f0ef34c5c6d2bb4a94284ea6070a5ae4f8601
mackarpov <mackarpov@gmail.com>
2025-11-22 18:52:33 +0300

    KS-11-1. Создать класс с данными пользователя и создать 2 экземпляра класса.

---
+++ src/main/kotlin/lesson_11/main.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_11
+
+fun main() {
+
+    val user1 = User(
+        userId = 1,
+        userLogin = "Login1",
+        userPassword = "Password1",
+        userEmail = "user1@gmail.com"
+    )
+
+    val user2 = User(
+        userId = 2,
+        userLogin = "Login2",
+        userPassword = "Password2",
+        userEmail = "user2@gmail.com"
+        )
+
+    println("User1 Id = ${user1.userId}")
+    println("User1 Login = ${user1.userLogin}")
+    println("User1 Password = ${user1.userPassword}")
+    println("User1 Email = ${user1.userEmail}")
+
+    println()
+
+    println("User2 Id = ${user2.userId}")
+    println("User2 Login = ${user2.userLogin}")
+    println("User2 Password = ${user2.userPassword}")
+    println("User2 Email = ${user2.userEmail}")
+}
