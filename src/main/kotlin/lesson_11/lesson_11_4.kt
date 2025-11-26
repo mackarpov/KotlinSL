a120c67ca5679f4964a9c581770f32224fdf492c
mackarpov <mackarpov@gmail.com>
2025-11-26 21:04:37 +0300

    KS-11-4. Проектирование сайта с рецептами. Убрал методы, добавил поля с описанием и ссылками на изображения.

--- src/main/kotlin/lesson_11/lesson_11_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_4.kt
@@ -1,43 +1,21 @@
 package org.example.lesson_11
 
 class Ingredient(
-    val ingredientTitle: String,
-    val amount: Double,
+    val title: String,
+    val amount: String,
     val unitOfMeasurement: String,
 )
 
 class Recipe(
-    val recipeTitle: String,
-    val listOfIngredients: MutableList<Ingredient> = mutableListOf(),
-) {
-    fun addIngredient(ingredient: Ingredient) {
-        listOfIngredients += ingredient
-    }
-}
+    val title: String,
+    val listOfIngredients: List<Ingredient> = emptyList(),
+    val imageURL: String,
+    val description: String,
+)
 
 class Category(
-    val categoryTitle: String,
-    val listOfRecipes: MutableList<Recipe> = mutableListOf(),
-) {
-    fun addRecipe(recipe: Recipe) {
-        listOfRecipes += recipe
-    }
-}
-
-
-fun main() {
-
-    val potato = Ingredient("картофель", 1.0, "кг.")
-    val milk = Ingredient("молоко", 0.5, "кг.")
-    val butter = Ingredient("масло сливочное", 50.0, "гр.")
-
-    val mashedPotato = Recipe("Картофель пюре")
-    mashedPotato.addIngredient(potato)
-    mashedPotato.addIngredient(milk)
-    mashedPotato.addIngredient(butter)
-
-    val lunches = Category("Обеды")
-    lunches.addRecipe(mashedPotato)
-
-
-}
+    val title: String,
+    val listOfRecipes: List<Recipe> = emptyList(),
+    val imageURL: String,
+    val description: String,
+)
